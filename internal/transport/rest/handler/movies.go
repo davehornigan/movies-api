96ef6c0147b7afa3679412c1184b0956fad176d3
Dave Hornigan <dave@hornigan.com>
2023-10-07 04:32:04 +0300

    Updated movies list api

--- internal/transport/rest/handler/movies.go
+++ internal/transport/rest/handler/movies.go
@@ -3,42 +3,111 @@ package handler
 import (
 	"github.com/davehornigan/movies-api/generated/tmdb"
 	"github.com/labstack/echo/v4"
-	"github.com/sirupsen/logrus"
 	"net/http"
 )
 import apiserver "github.com/davehornigan/movies-api/generated/api-server"
 
 func (h *Handler) GetMoviesListType(c echo.Context, listType apiserver.MovieListType, params apiserver.GetMoviesListTypeParams) error {
-	var response *http.Response
-	var err error
 	if listType == apiserver.Popular {
-		requestParams := tmdb.GetMoviePopularPaginatedParams{
-			Page:     params.Page,
-			Language: params.Language,
-			Region:   params.CountryCode,
-		}
-		response, err = h.tmdbClient.GetMoviePopularPaginated(c.Request().Context(), &requestParams)
+		return GetPopularMovies(h, c, params)
 	} else if listType == apiserver.Upcoming {
-		requestParams := tmdb.GetMovieUpcomingPaginatedParams{
-			Page:     params.Page,
-			Language: params.Language,
-			Region:   params.CountryCode,
-		}
-		response, err = h.tmdbClient.GetMovieUpcomingPaginated(c.Request().Context(), &requestParams)
-	} else {
-		requestParams := tmdb.GetMovieTopRatedPaginatedParams{
-			Page:     params.Page,
-			Language: params.Language,
-			Region:   params.CountryCode,
-		}
-		response, err = h.tmdbClient.GetMovieTopRatedPaginated(c.Request().Context(), &requestParams)
+		return GetUpcomingMovies(h, c, params)
 	}
+
+	return GetTopRatedMovies(h, c, params)
+}
+
+func GetPopularMovies(h *Handler, c echo.Context, params apiserver.GetMoviesListTypeParams) error {
+	requestParams := tmdb.GetMoviePopularPaginatedParams{
+		Page:     params.Page,
+		Language: params.Language,
+		Region:   params.CountryCode,
+	}
+	r, err := h.tmdbClient.GetMoviePopularPaginated(c.Request().Context(), &requestParams)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error()) // TODO: Customize error
+	}
+	response, err := tmdb.ParseGetMoviePopularPaginatedResponse(r)
 
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error()) // TODO: Customize error
 	}
 
-	logrus.Info(response)
+	if response.StatusCode() != http.StatusOK {
+		return c.JSON(response.StatusCode(), "External error")
+	}
 
-	return c.String(http.StatusOK, "MovieList")
+	return c.JSON(http.StatusOK, BuildMovieList(response.JSON200))
+}
+
+func GetUpcomingMovies(h *Handler, c echo.Context, params apiserver.GetMoviesListTypeParams) error {
+	requestParams := tmdb.GetMovieUpcomingPaginatedParams{
+		Page:     params.Page,
+		Language: params.Language,
+		Region:   params.CountryCode,
+	}
+	r, err := h.tmdbClient.GetMovieUpcomingPaginated(c.Request().Context(), &requestParams)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error()) // TODO: Customize error
+	}
+	response, err := tmdb.ParseGetMovieUpcomingPaginatedResponse(r)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error()) // TODO: Customize error
+	}
+
+	if response.StatusCode() != http.StatusOK {
+		return c.JSON(response.StatusCode(), "External error")
+	}
+
+	return c.JSON(http.StatusOK, BuildMovieList(response.JSON200))
+}
+
+func GetTopRatedMovies(h *Handler, c echo.Context, params apiserver.GetMoviesListTypeParams) error {
+	requestParams := tmdb.GetMovieTopRatedPaginatedParams{
+		Page:     params.Page,
+		Language: params.Language,
+		Region:   params.CountryCode,
+	}
+	r, err := h.tmdbClient.GetMovieTopRatedPaginated(c.Request().Context(), &requestParams)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error()) // TODO: Customize error
+	}
+	response, err := tmdb.ParseGetMovieUpcomingPaginatedResponse(r)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error()) // TODO: Customize error
+	}
+
+	if response.StatusCode() != http.StatusOK {
+		return c.JSON(response.StatusCode(), "External error")
+	}
+
+	return c.JSON(http.StatusOK, BuildMovieList(response.JSON200))
+}
+
+func BuildMovieList(resp *tmdb.MoviePaginated) apiserver.MovieListResponse {
+	movieList := make([]apiserver.MovieShortResponse, 0)
+	for _, movie := range *resp.Results {
+		movieList = append(movieList, apiserver.MovieShortResponse{
+			Duration:    0,
+			Id:          *movie.Id,
+			IsAdult:     *movie.Adult,
+			Poster:      movie.PosterPath,
+			Rating:      float64(*movie.Popularity),
+			ReleaseDate: nil,
+			Title:       *movie.Title,
+		})
+	}
+	listResponse := apiserver.MovieListResponse{
+		Items:      movieList,
+		Page:       *resp.Page,
+		TotalItems: *resp.TotalResults,
+		TotalPages: *resp.TotalPages,
+	}
+
+	return listResponse
 }
